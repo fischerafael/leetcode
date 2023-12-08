a762d390ed22ab23ce2537d68155f0d8c0aed97b
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-12-07 21:06:53 -0300

    feat: made with optmial

--- pages/api/longest-common-prefix.ts
+++ pages/api/longest-common-prefix.ts
@@ -2,8 +2,14 @@ import { isPalindrome } from "@/src/server/easy/palindrome-number";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const response = longestCommonPrefix(req.body.strings);
-  return res.status(200).json({ result: response });
+  const res1 = longestCommonPrefix(req.body.strings);
+  const res2 = longestCommonPrefixOptimal(req.body.strings);
+  return res.status(200).json({
+    result: {
+      myAlgo: res1,
+      optimal: res2,
+    },
+  });
 }
 
 export const longestCommonPrefix = (strings: string[]): string => {
@@ -40,4 +46,23 @@ const getSubstrings = (strings: string[], untilIndex: number): string[] => {
   return strings.map((string) => string.slice(0, untilIndex));
 };
 
-// "strings": ["flow", "flower", "flight"]
+const longestCommonPrefixOptimal = (strings: string[]): string => {
+  const sorted = sortWordsAlphabetically(strings);
+  let output = [];
+  const firstWord = sorted[0];
+  const lastWord = sorted[sorted.length - 1];
+  for (let i = 0; i < firstWord.length; i++) {
+    const isTheSameLetter = firstWord[i] === lastWord[i];
+    if (isTheSameLetter) {
+      output.push(firstWord[i]);
+    } else {
+      break;
+    }
+  }
+  const word = output.join("");
+  return word;
+};
+
+const sortWordsAlphabetically = (words: string[]): string[] => {
+  return words.sort((a, b) => (a < b ? -1 : 1));
+};
