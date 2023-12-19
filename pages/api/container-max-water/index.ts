506e954b7db11410436783c1c88a006a3c8977e2
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-12-19 18:22:22 -0300

    feat: calculating stuff

--- pages/api/container-max-water/index.ts
+++ pages/api/container-max-water/index.ts
@@ -5,5 +5,36 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 }
 
 const maxArea = (h: number[]): number => {
-  return 10;
+  return getBiggestVolumeFromIndex(h);
+  //   return calculateVolume(4, 3, 10);
+};
+
+const getBiggestVolumeFromIndex = (
+  hNums: number[],
+  initialIndex: number = 1
+): number => {
+  let biggest = 0;
+  let distance = 0;
+  for (let i = initialIndex; i < hNums.length; i++) {
+    const firstItem = hNums[initialIndex];
+    const currentItem = hNums[i];
+    const currentVolume = calculateVolume(firstItem, currentItem, distance);
+    const isBiggerThanBiggest = currentVolume > biggest;
+    if (isBiggerThanBiggest) {
+      biggest = currentVolume;
+    }
+
+    console.log("first h", firstItem);
+    console.log("second h", hNums[i]);
+    console.log("distance", distance);
+    console.log("volumes", currentVolume);
+
+    distance++;
+  }
+  return biggest;
+};
+
+const calculateVolume = (aH: number, bH: number, distance: number) => {
+  if (bH > aH) return aH * distance;
+  return bH * distance;
 };
