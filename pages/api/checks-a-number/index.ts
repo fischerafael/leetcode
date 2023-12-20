455a7b3847a6ef18c21d9469699c74cf7d78c88b
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-12-20 18:00:38 -0300

    feat: changed to seven

--- pages/api/checks-a-number/index.ts
+++ pages/api/checks-a-number/index.ts
@@ -1,11 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const response = checksANumber(req.body.array);
+  const response = checksIfThereIsASeven(req.body.array);
   return res.status(200).json({ result: response });
 }
 
-const checksANumber = (array: number[]): boolean => {
+const checksIfThereIsASeven = (array: number[]): boolean => {
   const arrString = array.join("");
   if (arrString.includes("7")) return true;
   return false;
